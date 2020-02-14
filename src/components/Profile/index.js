1a515d92feb5e5099f023123f88e9cfab4ed445c
Leonardo <leonardo_camargo31@hotmail.com>
2020-02-14 15:16:36 -0300

    created component profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile  = ()=>(
+  <div className='profile-wrapper'>
+    <h1>Leonardo Camargo</h1>
+    <h2>Fullstack developer</h2>
+    <p>Membro da empresa xxxx, escrevo sobre o mundo da programação e outras coisas mais.</p>
+  </div>
+)
+
+export default Profile
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React from 'react'
+// 1 - alem de não dar o refresh, e manter aquele padrão de usabilidade de um spa
+// 2 - no hover ele já vai carregar os dados em segundo plano, fazendo um 'prefetch' da outra página
+// assim quando a pessoa clicar vai abri muito rápido
+// 3 - activeStyle página que estou ativo
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import {Link} from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home</h1>
+    <SEO title='Home' />
+    <h1>Home</h1> 
+    <ul>
+      <li>
+        <Link to='/' activeStyle={{color:'red'}}>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
