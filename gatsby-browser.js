01892a229d62fc3998fc824b5d82fffda92cc75e
Leonardo <leonardo_camargo31@hotmail.com>
2020-02-28 16:45:33 -0300

    add lib prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // o gatsby-browser serve para importar bibliotecas,
 // para lidar com coisas de js que queremos fazer fora do react
 import 'lazysizes';
+
+// para mais temas https://prismjs.com/
+require('prismjs/themes/prism-okaidia.css');
